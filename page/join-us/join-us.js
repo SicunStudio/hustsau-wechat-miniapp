9086de48440a3ab07137b98c7d0830c3a6f8734c
smdsbz <694066681@qq.com>
2018-03-18 22:56:21 +0800

    need jQuery help

--- page/join-us/join-us.js
+++ page/join-us/join-us.js
@@ -7,7 +7,52 @@ Page({
    */
   data: {
     startDate: "1990-01-01",
-    today: new Date().toJSON().slice(0, 10)
+    today: new Date().toJSON().slice(0, 10),
+    departments: ['请选择...', '财务部', '秘书部', '人力资源部', '社团部', '行政监察部',
+        '公共关系部', '外联部', '媒体部', '宣传部', '思存工作室',
+        '新媒体工作室', '文艺拓展部', '社团外联企划部'
+    ],
+    birthday: new Date(new Date().getFullYear() - 18, 0, 1) // smdsbz: assuming they are all 18-years old
+                  .toJSON().slice(0, 10),
+    firstChoice: 0,     // smdsbz: pass `departments[firstChoice]` to back-end
+    secondChoice: 0,    //         `-1` for no second choice
+  },
+
+  /**
+   * invoked when user changed pickers in front-end
+   *   and YES, you have to change the displayed content **MANUALLY**
+   *
+   * Author:    smdsbz@GitHub.com
+   *
+   * Args:      e   - the picker element that triggered this event
+   *
+   * Return:    None
+   */
+  bindBirthdayChange: function (e) {
+    this.setData({
+        birthday: e.detail.value
+    });
+  },
+
+  bindRegionChange: function (e) {
+    this.setData({
+        birthPlace: e.detail.value
+    });
+  },
+
+  bindDepartmentChange: function (e) {
+    if (this.data.firstChoice != 0  // smdsbz: HACK: Don't delete `this`!
+          && e.detail.value == 0) {
+      // smdsbz: you cannot choose ‘请选择...’ again, once you chose sth. else
+      return;
+    }
+    this.setData({
+        firstChoice: e.detail.value
+    });
+  },
+
+  bindAlterChange: function (e) {
+
   },
 
   /**
