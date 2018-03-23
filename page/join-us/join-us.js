93f61271a2db056d92c56b551fac0b70cb14d5b3
smdsbz <694066681@qq.com>
2018-03-24 00:07:10 +0800

    finished join-us page basics, waiting for back-end RESTful API

--- page/join-us/join-us.js
+++ page/join-us/join-us.js
@@ -51,6 +51,76 @@ Page({
     });
   },
 
+  /**
+   * submit to back-end data server
+   *   NOTE: We don't have a data server at the time
+   *   TODO: Apply for a new server after MVP is shown
+   *
+   * Author:    smdsbz@GitHub
+   *
+   * Args:      e   - the form that is being submitted
+   *
+   * Return:    form data in JSON
+   */
+  submitApplicantionForm: function (e) {
+    var formdata = e.detail.value;
+    console.log(formdata);
+    // Data legality checks
+    // - empty name field
+    // ADDITIONAL: Regexp check
+    if (formdata["name"].length === 0) {
+      wx.showModal({
+        title: "信息不完整",
+        content: "请填写您的姓名！",
+        showCancel: false,
+        confirmText: "回去修改"
+      });
+      return;
+    }
+    // - refuse to give gender
+    if (formdata["gender"].length === 0) {
+      wx.showModal({
+        title: "信息不完整",
+        content: "请填写您的性别！",
+        showCancel: false,
+        confirmText: "回去修改"
+      });
+      return;
+    }
+    // - refuse to provide tel.
+    if (formdata["mobile"].length === 0) {
+      wx.showModal({
+        title: "信息不完整",
+        content: "请填写您的手机号码！我们将以短信的形式通知您面试地点！",
+        showCancel: false,
+        confirmText: "回去修改"
+      });
+      return;
+    }
+    // - choose one department at least
+    if (formdata["first-department-choice"] === 0) {
+      wx.showModal({
+        title: "信息不完整",
+        content: "请选择您要加入的部门！",
+        showCancel: false,
+        confirmText: "回去修改"
+      });
+      return;
+    }
+    // - if chose alternative department
+    if (formdata["allow-alternative-department"] === true
+          && formdata["second-department-choice"] === 0) {
+      wx.showModal({
+        title: "信息不完整",
+        content: "请选择您的备选部门！\n或者取消勾选「是否服从调剂」！",
+        showCancel: false,
+        confirmText: "回去修改"
+      });
+      return;
+    }
+    console.log("Legit data! POST to back-end data server");
+  },
+
   bindAlterChange: function (e) {
 
   },
